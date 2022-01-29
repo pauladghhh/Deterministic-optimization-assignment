631498770f4ab3a9bb35a7940c35bf5d4d7e5dc9
pauladghhh <44963820+pauladghhh@users.noreply.github.com>
2022-01-29 11:41:41 +0100

    Add files via upload

---
+++ ConjugateGradient.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    double x = (-1.5), y = (-1);
+    double x_new, y_new, fk;
+    double d0 = -(-400*x*y + 400*x*x*x - 2 + 2*x);
+    double d1 = -(200*y - 200*x*x);
+    double alpha, sigma = 0.05, rho = 0.19, epsilon = 4;
+    double bk, d0_old, d1_old;
+    unsigned short int stop = 0, iter = 0;
+    while (stop == 0)
+    {
+        iter = iter + 1;
+        alpha = 1;
+        x_new = x + alpha*d0;
+        y_new = y + alpha*d1;
+        fk = ((100 * (y-x*x) * (y-x*x)) + ((1-x) * (1-x)));
+        while (((100 * (y_new-x_new*x_new) * (y_new-x_new*x_new)) + ((1-x) * (1-x))) > (fk + sigma*alpha*(d0*(-d0)+d1*(-d1))))
+        {
+            alpha = alpha*rho;
+            x_new = x + alpha*d0;
+            y_new = y + alpha*d1;
+        }
+        x = x_new;
+        y = y_new;
+        d0_old = d0;
+        d1_old = d1;
+        bk = (d0 * d0 + d1 * d1)/(d0_old * d0_old + d1_old * d1_old);
+        d0 = -(-400*x*y + 400*x*x*x - 2 + 2*x) + (bk*d0_old);
+        d1 = -(200*y - 200*x*x) + (bk*d1_old);
+        if ((fabs(-400*x*y + 400*x*x*x - 2 + 2*x) < epsilon) && (fabs(200*y - 200*x*x) < epsilon))
+        {
+            stop = 1;
+        }
+        if (iter > 10000)
+        {
+            stop = 1;
+        }
+    }
+    printf("X value: %f \n",x);
+    printf("Y value: %f \n",y);
+    printf("Number of iterations: %i \n",iter);
+    return 0;
+}
---
+++ SteepestDescent.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    double x = (-1.5), y = (-1);
+    double x_new, y_new, fk;
+    double d0 = (-400*x*y + 400*x*x*x - 2 + 2*x);
+    double d1 = (200*y - 200*x*x);
+    double alpha, sigma = 0.05, rho = 0.007605, epsilon = 4;
+    unsigned short int stop = 0, iter = 0;
+    while (stop == 0)
+    {
+        iter = iter + 1;
+        alpha = 1;
+        x_new = x - alpha*d0;
+        y_new = y - alpha*d1;
+        fk = ((100 * (y-x*x) * (y-x*x)) + ((1-x) * (1-x)));
+        while (((100 * (y_new-x_new*x_new) * (y_new-x_new*x_new)) + ((1-x) * (1-x))) > (fk + sigma*alpha*(d0*(-d0)+d1*(-d1))))
+        {
+            alpha = alpha*rho;
+            x_new = x - alpha*d0;
+            y_new = y - alpha*d1;
+        }
+        x = x_new;
+        y = y_new;
+        d0 = (-400*x*y + 400*x*x*x - 2 + 2*x);
+        d1 = (200*y - 200*x*x);
+        if ((fabs(d0) < epsilon) && (fabs(d1) < epsilon))
+        {
+            break;
+        }
+        if (iter > 10000)
+        {
+            stop = 1;
+        }
+    }
+    printf("X value: %f \n",x);
+    printf("Y value: %f \n",y);
+    printf("Number of iterations: %i \n",iter);
+    return 0;
+}
